2250e55d130e3f1f759feb06191bc64e7b64f192
bestauffer <77029920+bestauffer@users.noreply.github.com>
2021-01-18 20:37:16 -0800

    Add files via upload

--- SwitchLetters.js
+++ SwitchLetters.js
@@ -1,30 +1,70 @@
-let items = [];
-
-function addItems(){
-    for(let i=0; i<3; i++){
-        items[i] = window.prompt("Please Enter a Word:");
-    };   
+function getItems(){
+    localStorage.setItem('temp', 0);
     
-    window.location.href = "page2.html";
-    makeList();
+    let items = [];
+    for(let i=0; i<3; i++){
+        items[i] = window.prompt("Please Enter a Word:");       
+    }   
+    
+    localStorage.setItem('itemList', JSON.stringify(items));
+    
+
+    let secondArray = items.map(oneWord);
+    localStorage.setItem('switchedLetters', JSON.stringify(secondArray));
+    location.reload();
+
+    let newTemp = localStorage.getItem('temp');    
+    newTemp++;
+    localStorage.setItem('temp', newTemp);
+
+    document.getElementById('myP1').classList.toggle('myP1');
+
+
 }
 
 function makeList() {
+    
+    let newTemp = localStorage.temp;
+    if(newTemp != 0){
+  document.getElementById("myP1").style.visibility = "hidden";
+
+    }
+
+
+let retrieved = localStorage.getItem('itemList');
+
+let itemsLi = JSON.parse(retrieved);
 let ul = document.createElement('ul');
 document.getElementById('myItems').appendChild(ul);
-for(let i = 0; i < items.length; i++){
+for(let i = 0; i < itemsLi.length; i++){
     let li = document.createElement('li');
     ul.appendChild(li);
-    li.innerHTML = items[i];
-};
-
-
+    li.innerHTML = itemsLi[i];
 }
 
+localStorage.setItem('temp', 0);
+localStorage.removeItem('itemList')
+document.getElementById("myP2").style.visibility = "visible";
+}
+
+function makeOtherList() {
+    let retrieved = localStorage.getItem('switchedLetters');
+    
+    let itemsLi = JSON.parse(retrieved);
+    let ul = document.createElement('ul');
+    document.getElementById('myOtherItems').appendChild(ul);
+    for(let i = 0; i < itemsLi.length; i++){
+        let li = document.createElement('li');
+        ul.appendChild(li);
+        li.innerHTML = itemsLi[i];
+    }
+    
+    localStorage.removeItem('switchedLetters')
+    }
 
 
-let secondArray = items.map(oneWord);
 
 function oneWord (newWord){ 
     return newWord.charAt(newWord.length - 1) + newWord.substring(1, newWord.length - 1) + newWord.charAt(0); 
 }
+
